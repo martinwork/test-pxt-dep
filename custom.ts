4c31498cb8e30b09a14c4487e6aa21583653561a
Martin Williams <martinwork@users.noreply.github.com>
2025-11-30 13:31:03 +0000

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,14 @@
+/**
+ * Dep blocks
+ */
+//% weight=100 color=#ff0000 icon=""
+namespace testDep {
+    /**
+     * Square
+     * @param value the value to square
+     */
+    //% block
+    export function square(value: number): number {
+        return value * value;
+    }
+}
